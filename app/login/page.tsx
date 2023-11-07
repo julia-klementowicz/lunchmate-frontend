2b2820052e79ab9a1a86f3e7179d6fceac9a0b08
Marcin Buko <thedak@o2.pl>
2023-11-07 12:44:32 +0100

    Logowanie i rejestracja

---
+++ app/login/page.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import Link from 'next/link';
+
+export default function Login() {
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="w-full md:w-1/2 flex items-center justify-center p-4 md:p-8 mt-8 mb-4">
+        <div className="max-w-md space-y-4">
+          <h2 className="text-2xl font-bold text-center">Zaloguj się</h2>
+
+          <p className="text-center text-gray-500">Wprowadź swój adres email i hasło, aby się zalogować</p>
+
+          <form>
+            <div className="space-y-2">
+              <div>
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="name@example.com"
+                  className="w-full border p-2"
+                />
+              </div>
+              <div>
+                <Label htmlFor="password">Hasło</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="*******************"
+                  className="w-full border p-2"
+                />
+              </div>
+              <Button className="w-full bg-primary-yellow text-black py-2 rounded">
+                Zaloguj się
+              </Button>
+              <div className="flex items-center justify-center space-x-4">
+              <div className="flex-1 border-b"></div>
+              <div className="text-gray-400">Nie masz jeszcze konta?</div>
+              <div className="flex-1 border-b"></div>
+          </div>
+
+          <Button variant="outline" className="w-full text-black py-2 rounded">
+            Załóż konto
+          </Button>
+            </div>
+          </form>
+
+          <p className="text-center text-gray-500 mt-4 mb-2">
+            Kontynuując zgadzasz się na 
+            <Link href="https://pl.wikipedia.org/wiki/Regulamin" className="underline"> Regulamin </Link>
+            i 
+            <Link href="https://pl.wikipedia.org/wiki/Polityka_prywatno%C5%9ci" className="underline"> Politykę prywatności</Link>
+          </p>
+        </div>
+      </div>
+
+      <div className="hidden md:w-1/2 relative md:block">
+        <Image
+          src="/login.png"
+          alt="banner"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-none"
+        />
+      </div>
+    </div>
+  );
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,107 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import Link from 'next/link';
+
 export default function Register() {
   return (
-    <div className="bg-primary-yellow">siema</div>
-  )
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="hidden md:w-1/2 relative md:block">
+        <Image
+          src="/register.png"
+          alt="banner"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-none"
+        />
+      </div>
+
+      <div className="w-full md:w-1/2 flex items-center justify-center p-4 md:p-8 mt-8 mb-4">
+        <div className="max-w-md space-y-4">
+          <h2 className="text-2xl font-bold text-center">Załóż konto</h2>
+
+          <p className="text-center text-gray-500">Wprowadź swoje dane, aby założyć konto</p>
+
+          <form>
+            <div className="space-y-2">
+              <div>
+                <Label htmlFor="firstName">Imię</Label>
+                <Input
+                  type="text"
+                  id="firstName"
+                  placeholder="Anna"
+                  className="w-full border p-2"
+                />
+              </div>
+              <div>
+                <Label htmlFor="lastName">Nazwisko</Label>
+                <Input
+                  type="text"
+                  id="lastName"
+                  placeholder="Kowalska"
+                  className="w-full border p-2"
+                />
+              </div>
+              <div>
+                <Label htmlFor="birthdate">Data urodzenia</Label>
+                <Input
+                  type="date"
+                  id="birthdate"
+                  placeholder="12/04/1998"
+                  className="w-full border p-2"
+                />
+              </div>
+              <div>
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="m@example.com"
+                  className="w-full border p-2"
+                />
+              </div>
+              <div>
+                <Label htmlFor="login">Login</Label>
+                <Input
+                  type="text"
+                  id="login"
+                  placeholder="example"
+                  className="w-full border p-2"
+                />
+              </div>
+              <div>
+                <Label htmlFor="password">Hasło</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="************"
+                  className="w-full border p-2"
+                />
+              </div>
+              <Button className="w-full bg-primary-yellow text-black py-2 rounded">
+                Załóż konto
+              </Button>
+            </div>
+          </form>
+
+          <div className="flex items-center justify-center space-x-4">
+            <div className="flex-1 border-b"></div>
+            <div className="text-gray-400">LUB</div>
+            <div className="flex-1 border-b"></div>
+          </div>
+
+          <Button variant="outline" className="w-full text-black py-2 rounded">Zaloguj się</Button>
+
+          <p className="text-center text-gray-500 mt-4 mb-2">
+            Kontynuując zgadzasz się na 
+            <Link href="https://pl.wikipedia.org/wiki/Regulamin" className="underline"> Regulamin </Link>
+            i  
+            <Link href="https://pl.wikipedia.org/wiki/Polityka_prywatno%C5%9ci" className="underline"> Politykę prywatności</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
