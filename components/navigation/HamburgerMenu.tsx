330de4a39a0e44126e02fd07e1d338aff0ab9991
julia-klementowicz <julia.klementowicz1@gmail.com>
2023-11-12 18:45:32 +0100

    added responsive mobile design to homepage

---
+++ components/navigation/HamburgerMenu.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
+
+export default function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+  return (
+    <>
+      <button onClick={toggleDrawer}>
+        <Image
+          src='/hamburger_menu_icon.svg'
+          alt='open mobile menu'
+          width={36}
+          height={36}
+        />
+      </button>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction='left'
+        duration={360}
+        style={{ width: '100%' }}
+        zIndex={1000}
+        lockBackgroundScroll
+      >
+        <div className='w-full flex flex-col gap-6'>
+          <div className='w-full h-14 px-4 py-2 grid grid-cols-3 border-b'>
+            <div className=''></div>
+            <div className='flex items-center justify-center gap-2'>
+              <Image
+                src='/logo_icon.svg'
+                alt='LunchMate logo'
+                width={25}
+                height={25}
+              />
+              <h1 className='font-medium text-xl'>LunchMate</h1>
+            </div>
+            <button className='justify-self-end' onClick={toggleDrawer}>
+              <Image
+                src='/close_icon.svg'
+                alt='close mobile menu'
+                width={34}
+                height={34}
+              />
+            </button>
+          </div>
+          <ul className='flex flex-col justify-center items-center gap-4 mt-[2px] text-lg font-medium'>
+            <li>
+              <Link href='/' onClick={toggleDrawer}>
+                Strona główna
+              </Link>
+            </li>
+            <li>
+              <Link href='/' onClick={toggleDrawer}>
+                Przepisy
+              </Link>
+            </li>
+            <li>
+              <Link href='/about' onClick={toggleDrawer}>
+                O nas
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Drawer>
+    </>
+  );
+}
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,28 +1,68 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Input } from '../ui/input';
+import HamburgerMenu from './HamburgerMenu';
 
 export default function Navbar() {
+  const [showSearch, setShowSearch] = useState(false);
   return (
-    <header className='w-full h-14 px-4 flex justify-between items-center border-b border-neutral-200'>
-      <div className='flex gap-2 w-2/12 lg:w-3/12 max-w-[300px]'>
-        <Image src='/logo_icon.svg' alt='LunchMate logo' width={25} height={25} />
-        <h1 className='font-medium text-xl'>LunchMate</h1>
-      </div>
-      <ul className='flex justify-center items-center gap-8 mt-[2px] font-medium'>
-        <li>
-          <Link href='/'>Strona główna</Link>
-        </li>
-        <li>
-          <Link href='/'>Przepisy</Link>
-        </li>
-        <li>
-          <Link href='/about'>O nas</Link>
-        </li>
-      </ul>
-      <div className='w-2/12 lg:w-3/12 max-w-[300px]'>
-        <Input type='text' placeholder='Szukaj...' className='h-9' />
-      </div>
-    </header>
+    <>
+      <header className='w-full h-14 px-4 hidden sm:flex justify-between items-center border-b border-neutral-200'>
+        <div className='flex gap-2 w-2/12 lg:w-3/12 max-w-[300px]'>
+          <Image
+            src='/logo_icon.svg'
+            alt='LunchMate logo'
+            width={25}
+            height={25}
+          />
+          <h1 className='font-medium text-xl'>LunchMate</h1>
+        </div>
+        <ul className='hidden sm:flex justify-center items-center gap-8 mt-[2px] font-medium'>
+          <li>
+            <Link href='/'>Strona główna</Link>
+          </li>
+          <li>
+            <Link href='/'>Przepisy</Link>
+          </li>
+          <li>
+            <Link href='/about'>O nas</Link>
+          </li>
+        </ul>
+        <div className='w-2/12 lg:w-3/12 max-w-[300px]'>
+          <Input type='text' placeholder='Szukaj...' className='h-9' />
+        </div>
+      </header>
+      <header className='w-full h-14 px-4 flex sm:hidden justify-between items-center border-b border-neutral-200'>
+        <div className='w-2/12 flex items-center'>
+          <HamburgerMenu />
+        </div>
+        <div className='flex items-center justify-center gap-2 w-4/12'>
+          <Image
+            src='/logo_icon.svg'
+            alt='LunchMate logo'
+            width={25}
+            height={25}
+          />
+          <h1 className='font-medium text-xl'>LunchMate</h1>
+        </div>
+        <div className='w-2/12 flex items justify-end relative'>
+          <button onClick={() => setShowSearch(!showSearch)}>
+            <Image src='/search_icon.svg' alt='search' width={30} height={30} />
+          </button>
+          <div className='absolute top-[44px] -right-2 z-10'>
+            <Input
+              type='text'
+              placeholder='Szukaj...'
+              className={`w-[200px] ${
+                showSearch ? '' : 'p-0 border-0 h-0'
+              } transition-all duration-200`}
+            />
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
